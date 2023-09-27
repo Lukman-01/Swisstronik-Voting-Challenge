dcc586824d949dedf8cfb9b03ed21c0cc4a4ec39
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-09-27 15:46:17 +0100

    hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
+
+const { PRIVATE_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/", 
+      accounts: [`0x${PRIVATE_KEY}`],
+    },
+  },
 };
